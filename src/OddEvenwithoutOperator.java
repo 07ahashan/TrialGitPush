ab36a47f4b33a1d32b276b2389785e97a3e313d6
Ahashan Ali <0786ahashanali@gmail.com>
2024-05-26 02:24:47 +0530

    Changes Commit

---
+++ src/OddEvenwithoutOperator.java
@@ -0,0 +1,12 @@
+public class OddEvenwithoutOperator {
+    public static void main(String[] args) {
+        int n=40;
+
+        if((n&1)==0)
+        {
+            System.out.println("Even"+ ":"+n);
+        }else {
+            System.out.println("Odd" + ":"+n);
+        }
+    }
+}
